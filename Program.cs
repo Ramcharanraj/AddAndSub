a4122e1bef2ea0fa17d58af7c95fd3b82f9f5efb
Ramcharanraj <86968389+Ramcharanraj@users.noreply.github.com>
2021-07-21 01:40:10 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace AdditionAndSubstraction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Num1, Num2;
+            Console.WriteLine("Enter the Num1");
+            Num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the Num2");
+            Num2 = Convert.ToInt32(Console.ReadLine());
+
+            int add, sub;
+
+            add = Num1 + Num2;
+            sub = Num1 - Num2;
+            Console.WriteLine("Sum of two numbers:" + add.ToString());
+            Console.WriteLine("The Substraction of two numbers:" + sub.ToString());
+        }
+    }
+}
